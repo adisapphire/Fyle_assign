ab40dfa66330769bcf4a795f457b4c1e8893d472
rockstar777 <puneetbindal8989@gmail.com>
2019-04-13 16:02:42 -0700

    lets deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Component0Component } from './component0/component0.component';
+import { Component4Component } from './component4/component4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component0Component,
+    Component4Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/component2/component2.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.scss']
+})
+export class Component2Component implements OnInit {
+
+  items = ['25','50','100','200'];
+  item = '25';
+  right = false;
+  type = "monthly";
+  typeprice = 1;
+  lolcost = Number(this.item) * this.typeprice;
+  icecost = 2*Number(this.item) * this.typeprice;
+    
+  calc(){
+    this.lolcost = Number(this.item) * this.typeprice;
+    this.icecost = 2*Number(this.item) * this.typeprice;
+  }
+  
+
+  toggle() {
+    if (this.right) {
+      this.type = "month"
+      this.typeprice = 1;
+      this.calc();
+      return 'right';
+    } else {
+      this.type = "year"
+      this.typeprice = 0.5;
+      this.calc();
+      return 'left';
+    }
+    
+  }
+
+  constructor(private el: ElementRef) {
+
+   }
+
+  ngOnInit() {
+  }
+
+  onclick(item){
+      this.item=item;
+      this.calc();
+    }
+}
